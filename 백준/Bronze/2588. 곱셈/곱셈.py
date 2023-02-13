e632e9bfc794c8fee247ab41b9ffe32fba3ff0de
kwonhyeongju <112923172+kwonhyeongju@users.noreply.github.com>
2023-02-13 16:25:38 +0900

    Update 곱셈.py

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -6,3 +6,6 @@ AxB1 = A*int(B[1])
 AxB0 = A*int(B[0])
 AxB = A*int(B)
 print(AxB2,AxB1,AxB0,AxB, sep ='\n')
+
+#배운점 : 문자열 B를 B[2] B[1] B[0] 으로 나눠 int()를 사용해 곱셈연산에 사용할 수 있다.
+#배운점 : print()안에서 sep 를 통해 출력형태를 정할 수 있다.
