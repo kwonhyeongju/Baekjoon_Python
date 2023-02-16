6d8969467750ce04755443d5f8e4c568dd020186
kwonhyeongju <112923172+kwonhyeongju@users.noreply.github.com>
2023-02-16 18:24:23 +0900

    Update 별 찍기 － 1.py

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
@@ -4,3 +4,7 @@ for i in range(1,N+1):
     for j in range(1,i+1) :
         print("*",end = "")
     print()
+    
+    ## 파이썬의 경우 문자열을 더하고 곱할 수 있다는 강점이 있고, 직관성이 있는 언어입니다.
+    ## for i in range(1,(N+1)):
+    ##     print("*" * i)
